863e4350870b549b0c077dc06d50c8ca68b88d31
nepalimohan <nepalimohan@outlook.com>
2022-08-01 15:52:27 +0545

    MadeDynamic

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Education, Experience, Profile
 
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html')
+    if request.method == "POST":
+        pass
+    profile =  Profile.objects.all()
+    education = Education.objects.all()
+    experience = Experience.objects.all()
+    data = {
+        'profile': profile,
+        'education': education,
+        'experience': experience,
+    }
+    return render(request, 'pages/home.html', data)
