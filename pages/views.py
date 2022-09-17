06b0153bcfe1b1afa35a10309823e5c5b92b6541
nepalimohan <nepalimohan@outlook.com>
2022-09-17 10:40:38 +0545

    EmailSendComplete

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Education, Experience, Profile, FrontEndSkill, BackEndSkill
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
-    if request.method == "POST":
-        pass
     profile =  Profile.objects.all()
     education = Education.objects.all()
     experience = Experience.objects.all()
@@ -20,3 +20,23 @@ def home(request):
         'back_skills' : back_skills,
     }
     return render(request, 'pages/home.html', data)
+
+def contact(request):
+    if request.method == "POST":
+        name =  request.POST['name']
+        email =  request.POST['email']
+        subject =  request.POST['subject']
+        message =  request.POST['message']
+
+    admin_info = User.objects.get(is_superuser= True)
+    admin_email = admin_info.email
+
+    send_mail(
+        'You have received message from ' + name + ' whose email is ' + email + ' regarding ' + subject,
+        message,
+        'nepali.mohan11@gmail.com',
+        [admin_email],
+        fail_silently=False,
+    )
+    
+    return redirect('home')
