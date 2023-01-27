d3214e8246ec6b68a8c7fc9abfb52af4483007fc
nepalimohan <nepalimohan@outlook.com>
2023-01-27 22:27:31 +0545

    DB added

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -146,7 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nepali.mohan11@gmail.com'
-EMAIL_HOST_PASSWORD = 'zaaehvytqhxyatda'
+EMAIL_HOST_PASSWORD = 'kglgmulwqkhgplcl'
 EMAIL_USE_TLS = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
